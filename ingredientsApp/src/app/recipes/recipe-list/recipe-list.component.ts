fa4885b9c81228dc3e8cdc7df62a7190fdb6f507
Jefferson <jedoljak@gmail.com>
2019-06-09 12:57:35 -0300

    chapter 3 finished

--- ingredientsApp/src/app/recipes/recipe-list/recipe-list.component.ts
+++ ingredientsApp/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes:Recipe[] = [
+    new Recipe('Teste Nome', 'Descrição Teste', 'https://www.seriouseats.com/2018/01/20171129-cannoli-vicky-wasik-11-1500x1125.jpg'),
+    new Recipe('Teste Nome', 'Descrição Teste', 'https://www.seriouseats.com/2018/01/20171129-cannoli-vicky-wasik-11-1500x1125.jpg')
+  ]
 
   constructor() { }
 
